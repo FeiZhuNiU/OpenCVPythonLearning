0287e2753b8750c8471e7881101f51e28a563c0d
Eric Yu <yulin.jay@gmail.com>
2018-07-19 18:25:54 +0800

    face detect

--- cvutils/utils.py
+++ cvutils/utils.py
@@ -204,6 +204,14 @@ class EmbossFilter(VConvolutionFilter):
         VConvolutionFilter.__init__(self, kernel)
 
 
+def detect_face(img):
+    face_cascade = cv2.CascadeClassifier('./cascades/haarcascades/haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
 if __name__ == "__main__":
     test = [1, 2, 3, 5, 7, 0]
     test.sort(reverse=True)
--- scratch.py
+++ scratch.py
@@ -18,9 +18,10 @@ class Scratch(object):
             frame = self._capture_manager.frame
             if frame is not None:
                 # TODO
-                self._capture_manager.bin_image = utils.convert2binary(frame, ksize=51)
-                self._capture_manager.bin_image = utils.edge_detection(self._capture_manager.bin_image)
-                self._capture_manager.frame = utils.detect_and_draw_lines(self._capture_manager.bin_image, frame)
+                # self._capture_manager.bin_image = utils.convert2binary(frame, ksize=51)
+                # self._capture_manager.bin_image = utils.edge_detection(self._capture_manager.bin_image)
+                # self._capture_manager.frame = utils.detect_and_draw_lines(self._capture_manager.bin_image, frame)
+                self._capture_manager.frame = utils.detect_face(frame)
             self._capture_manager.exit_frame()
             self._window_manager.process_events()
 
